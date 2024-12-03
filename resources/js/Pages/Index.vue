176800c6a659691a30843bbda738bc4de6fac847
Heang Virith <virith.id@gmail.com>
2024-12-03 21:57:08 +0700

    Add Inertia.js support with Vue 3 and Vuetify integration

---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1 class="bg-red-400">Index Page</h1>
+        <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                    card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        <v-carousel>
+            <v-carousel-item src="https://cdn.vuetifyjs.com/images/cards/docks.jpg" cover></v-carousel-item>
+
+            <v-carousel-item src="https://cdn.vuetifyjs.com/images/cards/hotel.jpg" cover></v-carousel-item>
+
+            <v-carousel-item src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" cover></v-carousel-item>
+        </v-carousel>
+    </div>
+</template>
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,3 +2,12 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+// import bootstrap
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap';
+
+
+
+
