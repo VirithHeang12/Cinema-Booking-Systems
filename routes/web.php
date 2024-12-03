a2af340ecd9b282fae25f6446f02d85f31e8b8e7
Heang Virith <virith.id@gmail.com>
2024-12-03 22:01:39 +0700

    Add CarController and Cars page with navigation from Index page

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\CarController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
     return Inertia::render('Index');
 });
+
+Route::resource('cars', CarController::class);
