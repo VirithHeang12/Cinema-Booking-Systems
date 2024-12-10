c809ff16057beb4af639f83a22f2e9a860e603c3
Heang Virith <virith.id@gmail.com>
2024-12-10 20:26:29 +0700

    Refactor AppServiceProvider: modularize boot method, configure Inertia, commands, models, and Vite settings

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 use Inertia\Inertia;
 use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
@@ -22,7 +25,19 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // share languages with Inertia
+        $this->configureInertia();
+        $this->configureCommands();
+        $this->configureModels();
+        $this->configureVite();
+    }
+
+    /**
+     * Configure Inertia to share data with all components in all requests.
+     *
+     * @return void
+     */
+    private function configureInertia(): void
+    {
         Inertia::share('languages', function () {
             $languages = [];
 
@@ -37,5 +52,33 @@ class AppServiceProvider extends ServiceProvider
 
             return $languages;
         });
+    }
+
+    /**
+     * Configure to prevent destructive commands from running in production.
+     *
+     * @return void
+     */
+    private function configureCommands(): void
+    {
+        DB::prohibitDestructiveCommands($this->app->environment('production'));
+    }
+
+    /**
+     * Configure Eloquent models to be strict.
+     */
+    private function configureModels(): void
+    {
+        Model::shouldBeStrict();
+    }
+
+    /**
+     * Configure Vite
+     *
+     * @return void
+     */
+    private function configureVite(): void
+    {
+        Vite::usePrefetchStrategy('aggressive');
     }
 }
