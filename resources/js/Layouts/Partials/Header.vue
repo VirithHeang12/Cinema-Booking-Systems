044a4dfd65c7da31583f043deee29af6597a8147
Heang Virith <virith.id@gmail.com>
2024-12-17 21:00:47 +0700

    Refactor Header component layout: enhance app bar with search field and additional buttons

--- resources/js/Layouts/Partials/Header.vue
+++ resources/js/Layouts/Partials/Header.vue
@@ -1,29 +1,41 @@
 <template>
-    <v-app-bar title="Application bar">
-        <v-menu>
-            <template v-slot:activator="{ props }">
-                <v-btn color="primary" dark v-bind="props">
-                    <lang-flag :iso="getLocale().toLowerCase()" />
-                </v-btn>
-            </template>
-
-            <v-list>
-                <v-list-item v-for="([key, value], index) in languages" :key="index">
-                    <v-list-item-title>
-                        <v-btn @click="switchLocale(key, value)">
-                            {{ value.native }}
+    <v-app-bar :elevation="0" color="grey-lighten-5" flat compact>
+        <v-container class="flex justify-between">
+            <div>
+                <v-text-field :width="200" :elevation="0"></v-text-field>
+            </div>
+            <div class="flex align-center justify-center">
+                <v-menu>
+                    <template v-slot:activator="{ props }">
+                        <v-btn color="primary" dark v-bind="props">
+                            <lang-flag :iso="getLocale().toLowerCase()" />
                         </v-btn>
-                    </v-list-item-title>
-                </v-list-item>
-            </v-list>
-        </v-menu>
-        <template v-slot:append>
-            <v-btn icon="mdi-heart"></v-btn>
+                    </template>
 
-            <v-btn icon="mdi-magnify"></v-btn>
+                    <v-list>
+                        <v-list-item v-for="([key, value], index) in languages" :key="index">
+                            <v-list-item-title>
+                                <v-btn @click="switchLocale(key, value)">
+                                    {{ value.native }}
+                                </v-btn>
+                            </v-list-item-title>
+                        </v-list-item>
+                    </v-list>
+                </v-menu>
+                <div class="flex gap-2 align-center">
+                    <v-btn>
+                        <template v-slot:prepend>
+                            <v-icon>mdi-ticket</v-icon>
+                        </template>
+                        Tickets
+                    </v-btn>
 
-            <v-btn icon="mdi-dots-vertical"></v-btn>
-        </template>
+                    <v-btn icon="mdi-account"></v-btn>
+
+                    <v-btn icon="mdi-bell"></v-btn>
+                </div>
+            </div>
+        </v-container>
     </v-app-bar>
 
 </template>
