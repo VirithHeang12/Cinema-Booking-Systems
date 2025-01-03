1c9fdd627fa958a045ce7a52689965bf40924e6e
Heang Virith <virith.id@gmail.com>
2025-01-03 21:44:43 +0700

    Add Genre and Movie models, controllers, factories, migrations, and seeders

---
+++ database/migrations/2025_01_03_143904_create_movies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Genre;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('genre_id')
+                ->constrained()
+                ->onDelete('set null')
+                ->onUpdate('cascade')
+                ->nullable(false);
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('movies');
+    }
+};
