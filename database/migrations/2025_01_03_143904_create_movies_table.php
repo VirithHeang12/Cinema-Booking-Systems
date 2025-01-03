ed64756f7b71c123a5161085874cd05777979768
Heang Virith <virith.id@gmail.com>
2025-01-03 21:45:56 +0700

    Remove soft deletes from movies table migration

--- database/migrations/2025_01_03_143904_create_movies_table.php
+++ database/migrations/2025_01_03_143904_create_movies_table.php
@@ -21,7 +21,6 @@ return new class extends Migration
                 ->onUpdate('cascade')
                 ->nullable(false);
 
-            $table->softDeletes();
             $table->timestamps();
         });
     }
