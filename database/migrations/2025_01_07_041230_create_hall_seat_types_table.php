40af72a8f9803133fb20edd797d794c7be2990ac
LaFii <sophannarith38@gmail.com>
2025-01-08 14:15:30 +0700

    Add controllers, models, factories, migrations, and seeders for Hall, Seat, HallType, and SeatType

---
+++ database/migrations/2025_01_07_041230_create_hall_seat_types_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('hall_seat_types', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('hall_id')
+                ->constrained()
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->foreignId('halltype_id')
+                ->constrained()
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->integer('maximun_capacity');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('hall_seat_types');
+    }
+};
