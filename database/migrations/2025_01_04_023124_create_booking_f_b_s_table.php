71bd380c770f4af296c25160c251a1e510e22c1f
HEANG SREYLEN <109011266+Sreylen096@users.noreply.github.com>
2025-01-08 21:15:39 +0700

    Len (#4)
    * Create table [payment_methods, food_and_beverages,bookings, booking_fb, show_seats]
    
    * [Modify] Add new column to BookingFB; create enums for BookingStatus and ShowSeatStatus
    
    ---------
    
    Co-authored-by: Lenning123 <joenlen321@gmail.com>

---
+++ database/migrations/2025_01_04_023124_create_booking_f_b_s_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('booking_f_b_s', function (Blueprint $table) {
+            $table->id();
+            $table->integer('quantity');
+            $table->decimal('amount', 10, 2);
+            $table->decimal('sale_unit_price', 10, 2);
+
+            $table->foreignId('booking_id')
+                ->nullable()
+                ->constrained('bookings')
+                ->onDelete('set null')
+                ->onUpdate('cascade');
+
+            $table->foreignId('fb_id')
+                ->nullable()
+                ->constrained('food_and_beverages')
+                ->onDelete('set null')
+                ->onUpdate('cascade');
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('booking_f_b_s');
+    }
+};
---
+++ database/seeders/FoodAndBeverageSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class FoodAndBeverageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
