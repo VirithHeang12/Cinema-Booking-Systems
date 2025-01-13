a4dacdb01854ddd6e6c24f09f237577228b596b9
Heang Virith <virith.id@gmail.com>
2025-01-13 21:37:51 +0700

    Add TestLayout component with footer integration

---
+++ resources/js/Layouts/TestLayout.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-app>
+        <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
+            <slot>
+                Main content
+            </slot>
+        </v-main>
+        <Footer />
+    </v-app>
+
+</template>
+
+<script setup>
+    import Footer from './Partials/Footer.vue';
+
+</script>
+
+<style scoped></style>
