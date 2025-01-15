12afb62ce20bd94844c72ee5b58c4b1de093a56c
Heang Sreylen <109011266+Sreylen096@users.noreply.github.com>
2025-01-15 11:57:04 +0700

    Create table (Language MovieSubtitle ScreenType Show) (#7)
    Co-authored-by: Cheth Chivy <chethchivy015@gmail.com>

---
+++ database/migrations/2025_01_12_090732_create_shows_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('shows', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('show_time')->nullable(false);
+            $table->enum('status', [
+                'Scheduled',
+                'Showing',
+                'Already',
+            ])->default('Scheduled');
+
+            $table->foreignId('movie_subtitle_id')
+                ->nullable(false)
+                ->constrained()
+                ->onDelete('set null')
+                ->onUpdate('cascade');
+
+            $table->foreignId('hall_id')
+                ->nullable(false)
+                ->constrained()
+                ->onDelete('set null')
+                ->onUpdate('cascade');
+            
+            $table->foreignId('screen_type_id')
+            ->nullable(false)
+            ->constrained()
+            ->onDelete('set null')
+            ->onUpdate('cascade');
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('shows');
+    }
+};
