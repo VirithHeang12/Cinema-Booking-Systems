0110d8e1c4fc4976943fc284d840222a2714aa55
Heang Virith <virith.id@gmail.com>
2025-01-20 22:01:39 +0700

    Implement Language management in Dashboard with CRUD operations

---
+++ resources/js/Pages/Dashboard/Languages/Create.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1>Create Language</h1>
+        <form @submit.prevent="submitForm">
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input type="text" v-model="form.name" class="form-control" id="name" name="name">
+            </div>
+            <div class="form-group">
+                <label for="code">Code</label>
+                <input type="text" v-model="form.code" class="form-control" id="code" name="code">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+</template>
+
+<script setup>
+    import { useForm } from '@inertiajs/vue3';
+
+    const form = useForm({
+        name: '',
+        code: '',
+    });
+
+    const submitForm = () => {
+        form.post(route('dashboard.languages.store'));
+    }
+
+</script>
---
+++ resources/js/Pages/Dashboard/Languages/Index.vue
@@ -0,0 +1,12 @@
+<template>
+    <button @click="createCallback">Create</button>
+</template>
+
+<script setup>
+    import { router } from '@inertiajs/vue3';
+
+    const createCallback = () => {
+        router.get(route('dashboard.languages.create'));
+    }
+
+</script>
