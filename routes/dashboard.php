0e14190776f055107e816904a160a997be2bfb26
Heang Virith <virith.id@gmail.com>
2025-01-22 21:45:05 +0700

    Refactor dashboard routes to consolidate language and classification resource definitions

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -5,8 +5,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Dashboard\ClassificationController;
 Route::prefix('dashboard')->name('dashboard.')->group(function () {
     Route::resource('languages', LanguageController::class);
-});
-
-Route::prefix('dashboard')->name('dashboard.')->group(function () {
     Route::resource('classifications', ClassificationController::class);
 });
+
