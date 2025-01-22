31dbde27188e24e52eb37edffa735eeab305fc44
Rachna-An <anrachna11@gmail.com>
2025-01-22 21:40:53 +0700

    Add Classification management to Dashboard with CRUD operations

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vuetify from 'vite-plugin-vuetify';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
         vuePlugin(),
@@ -14,4 +14,12 @@ export default defineConfig({
             autoImport: true,
         }),
     ],
+    server: {
+        cors: true, // Enables CORS
+        host: "localhost", // Ensures the dev server is accessible on localhost
+        port: 5173, // Ensure the correct port for the Vite dev server
+        headers: {
+            "Access-Control-Allow-Origin": "*", // Allow requests from any origin (use specific origin in production)
+        },
+    },
 });
