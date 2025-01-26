4d2865d697adfdaab69ef9d6f187f2782c38997d
HONG_SREYNEANG <hongsreyneang619@gmail.com>
2025-01-26 15:05:31 +0700

    Implement genre management: add create, edit, delete, and show functionalities with corresponding UI components

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -3,10 +3,13 @@
 use App\Http\Controllers\Dashboard\LanguageController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Dashboard\ClassificationController;
+use App\Http\Controllers\Dashboard\GenreController;
 Route::prefix('dashboard')->name('dashboard.')->group(function () {
     Route::get('languages/{language}/delete', [LanguageController::class, 'delete'])->name('languages.delete');
+    Route::get('genres/{genre}/delete', [GenreController::class, 'delete'])->name('genres.delete');
 
     Route::resource('languages', LanguageController::class);
     Route::resource('classifications', ClassificationController::class);
+    Route::resource('genres', GenreController::class);
 });
 
