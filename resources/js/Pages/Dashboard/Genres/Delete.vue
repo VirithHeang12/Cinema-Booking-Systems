0852636cf717a11fc3269050385170a25b4d610d
HONG SREYNEANG <167385495+SREYNEANG-HONG@users.noreply.github.com>
2025-01-27 16:14:12 +0700

    Sreyneang (#19)
    * modified composer and package
    
    * Add controllers, models, seeders, and migrations for Country, Classification, and Production Company
    
    * Add Language model, controller, factory, migration, and seeder; update Movie model and migration to include language_id
    
    * Remove Language model, controller, factory, migration, and seeder
    
    * Add controllers, models, and seeders for Show, Hall, Seat, Language, Promotion, and related entities; introduce ShowStatus enum
    
    * Add contact page with multilingual support and advertising partnership section
    
    * Refactor footer links to use Inertia.js Link component for improved navigation
    
    * Implement genre management: add create, edit, delete, and show functionalities with corresponding UI components
    
    ---------
    
    Co-authored-by: Heang Virith <121303816+VirithHeang12@users.noreply.github.com>

---
+++ resources/js/Pages/Dashboard/Genres/Delete.vue
@@ -0,0 +1,31 @@
+<template>
+    <Modal v-slot="{ close }">
+        <div>
+            <h1 class="text-lg font-semibold mb-4 text-primary-900 text-center">Are you sure you want to delete this ?</h1>
+   
+            <form @submit.prevent="submitForm">
+              
+                 
+                    <button @click="close" class="btn btn-dark text-white w-full mt-4 bg-primary-800 round-[10px] px-9 py-2 hover:bg-gray-800">Delete</button>
+                
+                
+            </form>
+        </div>
+    </Modal>
+</template>
+
+<script setup>
+    import { defineProps } from 'vue';
+    import { router } from '@inertiajs/vue3';
+
+    const props = defineProps({
+        genre: {
+            type: Object,
+            required: true,
+        }
+    });
+
+    const submitForm = () => {
+        router.delete(route('dashboard.genres.destroy', props.genre.id));
+    }
+</script>
--- routes/dashboard.php
+++ routes/dashboard.php
@@ -4,16 +4,19 @@ use App\Http\Controllers\Dashboard\LanguageController;
 use App\Http\Controllers\Dashboard\CountryController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Dashboard\ClassificationController;
+use App\Http\Controllers\Dashboard\GenreController;
 use App\Http\Controllers\Dashboard\HallTypeController;
 
 Route::prefix('dashboard')->name('dashboard.')->group(function () {
     Route::get('languages/{language}/delete', [LanguageController::class, 'delete'])->name('languages.delete');
     Route::get('countries/{country}/delete', [CountryController::class, 'delete'])->name('countries.delete');
     Route::get('halltypes/{halltype}/delete', [HallTypeController::class, 'delete'])->name('halltypes.delete');
+    Route::get('genres/{genre}/delete', [GenreController::class, 'delete'])->name('genres.delete');
   
     Route::resource('languages', LanguageController::class);
     Route::resource('countries', CountryController::class);
     Route::resource('classifications', ClassificationController::class);
+    Route::resource('genres', GenreController::class);
     Route::resource('halltypes', HallTypeController::class);
 });
 
