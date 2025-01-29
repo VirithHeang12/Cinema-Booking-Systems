f2b730eb593e055e7d4244d8a4b6e9d106e3161a
LaFii11 <134949278+LaFii11@users.noreply.github.com>
2025-01-29 18:04:47 +0700

    Add Privacy Policy page and related components; update language files (#22)

--- resources/js/Layouts/Partials/Footer.vue
+++ resources/js/Layouts/Partials/Footer.vue
@@ -5,42 +5,53 @@ import { Link } from '@inertiajs/vue3';
 
 <template>
     <footer class="bg-[#242424] px-4 relative overflow-y-hidden bg-blur z-1">
-        <v-container class="text-white pb-2">
+        <v-container class="pb-2 text-white">
             <div class="row border-b-[1.8px] border-white/[28%] z-10">
-                <div class="col-12 d-flex flex-col justify-center align-center">
+                <div class="flex-col justify-center col-12 d-flex align-center">
                     <img src="/public/logo/sample_logo.png" alt="" class="h-[56px]">
                     <h4 class="opacity-80">Eternal Cineplex</h4>
 
-                    <span class="text-center text-sm font-light opacity-60 leading-5">
+                    <span class="text-sm font-light leading-5 text-center opacity-60">
                         Your ultimate destination for movies and magic. <br>
-                        Discover the latest blockbusters, reserve your seats, and experience <br>unforgettable moments on the big screen — all from the comfort of your home.
+                        Discover the latest blockbusters, reserve your seats, and experience <br>unforgettable moments
+                        on the big screen — all from the comfort of your home.
                     </span>
                 </div>
-                <div class="col-12 flex flex-wrap justify-center align-center gap-x-20 gap-y-4 py-6">
-                    <a href="#" class="text-white text-decoration-none font-medium transition opacity-65 hover:opacity-100">Terms & Conditions</a>
-                    <a href="#" class="text-white text-decoration-none font-medium transition opacity-65 hover:opacity-100">Privacy policy</a>
-                    <Link href="/about" class="text-white text-decoration-none font-medium transition opacity-65 hover:opacity-100">About us</Link>
-                    <Link href="/contact" class="text-white text-decoration-none font-medium transition opacity-65 hover:opacity-100">Contact us</Link>
-                    <Link href="/locations" class="text-white text-decoration-none font-medium transition opacity-65 hover:opacity-100">Locations</Link>
+                <div class="flex flex-wrap justify-center py-6 col-12 align-center gap-x-20 gap-y-4">
+                    <a href="#"
+                        class="font-medium text-white transition text-decoration-none opacity-65 hover:opacity-100">Terms
+                        & Conditions</a>
+                    <Link href="/privacy"
+                        class="font-medium text-white transition text-decoration-none opacity-65 hover:opacity-100">
+                    Privacy & Policy</Link>
+                    <Link href="/about"
+                        class="font-medium text-white transition text-decoration-none opacity-65 hover:opacity-100">
+                    About us</Link>
+                    <Link href="/contact"
+                        class="font-medium text-white transition text-decoration-none opacity-65 hover:opacity-100">
+                    Contact us</Link>
+                    <Link href="/locations"
+                        class="font-medium text-white transition text-decoration-none opacity-65 hover:opacity-100">
+                    Locations</Link>
                 </div>
             </div>
 
-            <div class="row py-1 mt-2 z-10">
-                <div class="col-12 px-0 gap-y-4 d-flex flex-wrap justify-content-center justify-content-sm-between">
-                    <div class="col-12 col-sm-6 text-center text-sm-left">
-                        <span class="opacity-65 text-sm">© 2025 Eternal Cineplex. All rights reserved.</span>
+            <div class="z-10 py-1 mt-2 row">
+                <div class="flex-wrap px-0 col-12 gap-y-4 d-flex justify-content-center justify-content-sm-between">
+                    <div class="text-center col-12 col-sm-6 text-sm-left">
+                        <span class="text-sm opacity-65">© 2025 Eternal Cineplex. All rights reserved.</span>
                     </div>
-                    <div class="col-12 col-sm-6 flex justify-content-center justify-content-sm-end gap-4">
-                        <a href="https://www.facebook.com/" class="opacity-65 text-white hover:opacity-100 transition">
+                    <div class="flex gap-4 col-12 col-sm-6 justify-content-center justify-content-sm-end">
+                        <a href="https://www.facebook.com/" class="text-white transition opacity-65 hover:opacity-100">
                             <i class="bi bi-facebook"></i>
                         </a>
-                        <a href="https://www.instagram.com/" class="opacity-65 text-white hover:opacity-100 transition">
+                        <a href="https://www.instagram.com/" class="text-white transition opacity-65 hover:opacity-100">
                             <i class="bi bi-instagram"></i>
                         </a>
-                        <a href="https://x.com/" class="opacity-65 text-white hover:opacity-100 transition">
+                        <a href="https://x.com/" class="text-white transition opacity-65 hover:opacity-100">
                             <i class="bi bi-twitter-x"></i>
                         </a>
-                        <a href="https://telegram.org/" class="opacity-65 text-white hover:opacity-100 transition">
+                        <a href="https://telegram.org/" class="text-white transition opacity-65 hover:opacity-100">
                             <i class="bi bi-telegram"></i>
                         </a>
                     </div>
