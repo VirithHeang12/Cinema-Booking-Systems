15ec73df2ec0188b54908d2a8168fe2f7b38835f
Sreylen096 <heangsreylen096@gmail.com>
2025-02-10 20:59:18 +0700

    Add Country management API with validation and resource handling; implement CountryController and requests

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\V1\Dashboard\CountryController;
 use App\Http\Controllers\Api\V1\Dashboard\LanguageController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -11,6 +12,7 @@ Route::get('/user', function (Request $request) {
 Route::prefix('v1')->group(function () {
     Route::prefix('dashboard')->name('dashboard.')->group(function () {
         Route::apiResource('languages', LanguageController::class);
+        Route::apiResource('countries', CountryController::class);
     });
 })->middleware('auth:sanctum');
 
