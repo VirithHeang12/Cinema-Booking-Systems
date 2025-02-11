f4b97873611e75179d27b4855dd88fc70abad896
Rachna-An <anrachna11@gmail.com>
2025-02-11 21:57:35 +0700

    create api for classifications

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\V1\Dashboard\LanguageController;
+use App\Http\Controllers\Api\V1\Dashboard\ClassificationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -11,6 +12,7 @@ Route::get('/user', function (Request $request) {
 Route::prefix('v1')->group(function () {
     Route::prefix('dashboard')->name('dashboard.')->group(function () {
         Route::apiResource('languages', LanguageController::class);
+        Route::apiResource('classifications', ClassificationController::class);
     });
 })->middleware('auth:sanctum');
 
