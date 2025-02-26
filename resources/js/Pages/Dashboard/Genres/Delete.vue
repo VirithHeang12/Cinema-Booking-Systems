e123b4e4122e53d0f938604ae16a4bfa2a62342a
HONG SREYNEANG <167385495+SREYNEANG-HONG@users.noreply.github.com>
2025-02-26 11:32:36 +0700

    Refactor/migrate genre to server (#39)
    * Implement genre import and export functionality with Excel support
    
    * Add localization support for genre management UI components

--- resources/js/Pages/Dashboard/Genres/Delete.vue
+++ resources/js/Pages/Dashboard/Genres/Delete.vue
@@ -1,12 +1,12 @@
 <template>
     <Modal v-slot="{ close }">
         <div>
-            <h1 class="text-lg font-semibold mb-4 text-primary-900 text-center">Are you sure you want to delete this ?</h1>
+            <h1 class="text-lg font-semibold mb-4 text-primary-900 text-center">{{__('Are you want to delete this genre?')}}</h1>
    
             <form @submit.prevent="submitForm">
               
                  
-                    <button @click="close" class="btn btn-dark text-white w-full mt-4 bg-primary-800 round-[10px] px-9 py-2 hover:bg-gray-800">Delete</button>
+                    <button @click="close" class="btn btn-dark text-white w-full mt-4 bg-primary-800 round-[10px] px-9 py-2 hover:bg-gray-800">{{__('Delete')}}</button>
                 
                 
             </form>
