6e5f587867bd35d1d75079d0b2fdcb16883aca46
HONG SREYNEANG <167385495+SREYNEANG-HONG@users.noreply.github.com>
2025-03-06 20:42:01 +0700

    add column chart (#44)
    * Implement genre import and export functionality with Excel support
    
    * Add localization support for genre management UI components
    
    * Add column chart for movies per genre and clean up unused genre creation template
    
    ---------
    
    Co-authored-by: Heang Virith <121303816+VirithHeang12@users.noreply.github.com>

--- resources/js/Pages/Dashboard/Genres/Create.vue
+++ resources/js/Pages/Dashboard/Genres/Create.vue
@@ -1,22 +1,3 @@
-<!-- <template>
-    <Modal v-slot="{ close }">
-        <div>
-            <h1 class="text-lg font-semibold mb-4 text-primary-900 text-center">Create Genre</h1>
-            <hr class="my-2" />
-            <form @submit.prevent="submitForm">
-                <div class="form-group">
-                    <label for="name">Name</label>
-                    <input type="text" v-model="form.name" class="form-control" id="name" name="name">
-                </div>
-                <div class="form-group">
-                    <label for="description">Description</label>
-                    <input type="text" v-model="form.description" class="form-control" id="description" name="description">
-                </div>
-                <button type="submit" @click="close" class="btn btn-dark text-white w-full mt-4 bg-primary-800 round-[10px] px-9 py-2 hover:bg-gray-800">Submit</button>
-            </form>
-        </div>
-    </Modal>
-</template> -->
 <template>
     <Modal v-slot="{ close }">
         <div class="container mt-5 d-flex flex-column align-items-center">
