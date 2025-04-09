c66070aed6e5de337c08e080c94155e1e0d7c115
Chea Sophannarith <134949278+CheaSophannarith@users.noreply.github.com>
2025-04-09 20:43:39 +0700

    Hi (#62)
    * Enhance terms and conditions in language files and update Terms page to display detailed information
    
    * Implement screen type management features: add authorization policies, enhance import/export functionality, and improve UI components for better user experience.

--- app/Exports/ScreenTypesExport.php
+++ app/Exports/ScreenTypesExport.php
@@ -2,33 +2,144 @@
 
 namespace App\Exports;
 
+use App\Models\Movie;
 use App\Models\ScreenType;
-use Maatwebsite\Excel\Concerns\FromCollection;
-use Maatwebsite\Excel\Concerns\WithHeadings;
+use Illuminate\View\View;
+use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use Maatwebsite\Excel\Concerns\WithTitle;
+use PhpOffice\PhpSpreadsheet\Cell\Coordinate;
+use PhpOffice\PhpSpreadsheet\Style\Alignment;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class ScreenTypesExport implements FromCollection, WithHeadings
+class ScreenTypesExport implements FromView, WithStyles, ShouldAutoSize, WithTitle
 {
-    /**
-    * @return \Illuminate\Support\Collection
-    */
-    public function collection()
-    {
-        return ScreenType::select('id', 'name', 'description', 'created_at', 'updated_at')->get();
-    }
+    private $data;
 
-    /**
-     * Define the headings for the export file.
-     *
-     * @return array
-     */
-    public function headings(): array
+    public function __construct()
     {
-        return [
-            'ID',
-            'Name',
-            'Description',
-            'Created At',
-            'Updated At'
+       $screenTypes = ScreenType::all();
+
+        $this->data = [
+            'screenTypes'        => $screenTypes,
         ];
     }
+
+    /**
+     * Export from view
+     *
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function view(): View
+    {
+        return view('exports.screen_type', [
+            'screenTypes'        => $this->data['screenTypes'],
+        ]);
+    }
+
+    /**
+     * Set the sheet title
+     *
+     * @return string
+     */
+    public function title(): string
+    {
+        return 'ប្រភេទអេក្រង់';
+    }
+
+
+    /**
+     * Set the styles for the sheet
+     *
+     * @param Worksheet $sheet
+     *
+     * @return void
+     */
+    public function styles(Worksheet $sheet): void
+    {
+        $fontPrimary        = 'Khmer OS Siemreap';
+        $fontSecondary      = 'Khmer OS Muol';
+        $fontTertiary       = 'Khmer OS Siemreap';
+
+
+
+        $lastColumn         = $sheet->getHighestColumn();
+
+        /// last row
+        $lastRow            = count($this->data['screenTypes']) + 4;
+
+        // Define the array to store cell styles
+        $styles = [
+            // Header Section Style
+            'A1:' . $lastColumn . '1' => [
+                'font' => [
+                    'name' => $fontTertiary,
+                    'bold' => true,
+                    'size' => 16,
+                ],
+                'alignment' => [
+                    'horizontal'    => Alignment::HORIZONTAL_LEFT,
+                    'vertical'      => Alignment::VERTICAL_CENTER
+                ],
+            ],
+
+            // Header Section Style
+            'A2:' . $lastColumn . '2' => [
+                'font' => [
+                    'name' => $fontSecondary,
+                    'bold' => true,
+                    'size' => 14,
+                ],
+                'alignment' => [
+                    'horizontal'    => Alignment::HORIZONTAL_CENTER,
+                    'vertical'      => Alignment::VERTICAL_CENTER
+                ],
+            ],
+
+            /// Table Header Section Style
+            'A4:' . $lastColumn . '4' => [
+                'font' => [
+                    'name' => $fontPrimary,
+                    'bold' => true,
+                    'size' => 12,
+                ],
+                'alignment' => [
+                    'horizontal'    => Alignment::HORIZONTAL_CENTER,
+                    'vertical'      => Alignment::VERTICAL_CENTER
+                ],
+                'fill' => [
+                    'fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
+                ],
+                'borders' => [
+                    'allBorders' => [
+                        'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                    ],
+                ],
+            ],
+
+            /// Table Data Section Style
+            'A5:' . $lastColumn . $lastRow => [
+                'font' => [
+                    'name' => $fontPrimary,
+                    'size' => 12,
+                ],
+                'alignment' => [
+                    'horizontal'    => Alignment::HORIZONTAL_CENTER,
+                    'vertical'      => Alignment::VERTICAL_CENTER
+                ],
+                'borders' => [
+                    'allBorders' => [
+                        'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                    ],
+                ],
+            ],
+        ];
+
+        /// Apply styles to the worksheet
+        foreach ($styles as $range => $style) {
+            $sheet->getStyle($range)->applyFromArray($style);
+        }
+    }
 }
+
