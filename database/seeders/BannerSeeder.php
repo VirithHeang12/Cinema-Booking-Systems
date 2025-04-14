71e10615c55d873508a86da01cf1f127f3431324
Sreylen096 <heangsreylen096@gmail.com>
2025-04-14 23:14:24 +0700

    Implement on promotion and refactor style on [About, Contact, Privacy, Term]

--- database/seeders/BannerSeeder.php
+++ database/seeders/BannerSeeder.php
@@ -16,31 +16,36 @@ class BannerSeeder extends Seeder
         $banners = [
             [
                 'image_url'             => Storage::url('images/banner-1.jpg'),
+                'description'           => 'New Year Sale - Kick off 2021 with big discounts!',
                 'display_start_date'    => '2021-01-01 00:00:00',
                 'display_end_date'      => '2021-01-31 23:59:59',
             ],
             [
                 'image_url'             => Storage::url('images/banner-2.png'),
+                'description'           => 'Valentine’s Special Offers - Show your love with a gift!',
                 'display_start_date'    => '2021-02-01 00:00:00',
                 'display_end_date'      => '2021-02-28 23:59:59',
             ],
             [
                 'image_url'             => Storage::url('images/banner-3.jpg'),
+                'description'           => 'March Madness - Limited-time spring deals!',
                 'display_start_date'    => '2021-03-01 00:00:00',
                 'display_end_date'      => '2021-03-31 23:59:59',
             ],
             [
                 'image_url'             => Storage::url('images/banner-5.jpg'),
+                'description'           => 'April Clearance - Up to 50% off on selected items!',
                 'display_start_date'    => '2021-04-01 00:00:00',
                 'display_end_date'      => '2021-04-30 23:59:59',
             ],
             [
                 'image_url'             => Storage::url('images/banner-6.jpg'),
+                'description'           => 'Spring Collection Launch - Fresh styles now available!',
                 'display_start_date'    => '2021-04-01 00:00:00',
                 'display_end_date'      => '2021-04-30 23:59:59',
             ],
-
         ];
+
 
         foreach ($banners as $banner) {
             Banner::create($banner);
